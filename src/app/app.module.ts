8d0c857ab555a88a9899f5a0ff1c683ce47f0248
Trainer Mangai <mangai2000@gmail.com>
2023-10-01 19:00:57 +0530

    online-ticket-booking-app

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BusListComponent } from './components/bus/bus-list/bus-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BusRouteListComponent } from './components/bus/bus-route-list/bus-route-list.component';
+import { BusSeatViewComponent } from './components/bus/bus-seat-view/bus-seat-view.component';
+import { ReservationSuccessComponent } from './components/reservation-success/reservation-success.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BusListComponent,
+    BusRouteListComponent,
+    BusSeatViewComponent,
+    ReservationSuccessComponent
+  ],  
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
